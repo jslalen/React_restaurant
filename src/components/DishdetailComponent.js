7ed0285db63a0929d4a17fc5b252c08122042e57
jslalen <50656151+jslalen@users.noreply.github.com>
2020-06-20 18:45:30 +0530

    server communication

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Button, Row, Col, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderComments({comments, addComment, dishId}) {
     const com= comments.map((comm) => {
@@ -27,7 +28,7 @@ function RenderComments({comments, addComment, dishId}) {
 function RenderDish({dish}) {
     return (
             <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
